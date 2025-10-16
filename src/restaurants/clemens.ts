24b09d64fdad42a3b6c76f14251d6b73944720ef
Carl Fagerlin <carl.fagerlin@assaabloy.com>
2025-10-16 12:46:05 +0200

    lint

--- src/restaurants/clemens.ts
+++ src/restaurants/clemens.ts
@@ -1,61 +1,61 @@
-import type { Restaurant } from "./restaurant";
+import type { Restaurant } from "./restaurant"
 
 const weekdayMapping: Record<string, string> = {
-	man: "mon",
-	tis: "tue",
-	ons: "wed",
-	tor: "thu",
-	fre: "fri",
-};
+  man: "mon",
+  tis: "tue",
+  ons: "wed",
+  tor: "thu",
+  fre: "fri",
+}
 
 interface LunchmenyItem {
-	title: {
-		rendered: string;
-	};
-	acf: {
-		veckodag: Array<{
-			value: string;
-			label: string;
-		}>;
-	};
+  title: {
+    rendered: string
+  }
+  acf: {
+    veckodag: Array<{
+      value: string
+      label: string
+    }>
+  }
 }
 
 export class Clemens implements Restaurant {
-	private menu: Record<string, string> = {};
-	public restaurantName = "Clemens Kött & Husman";
-	public url = "https://olleburl.in/clemens/wp-json/wp/v2/lunchmeny";
-	public menuType = "weekly";
+  private menu: Record<string, string> = {}
+  public restaurantName = "Clemens Kött & Husman"
+  public url = "https://olleburl.in/clemens/wp-json/wp/v2/lunchmeny"
+  public menuType = "weekly"
 
-	constructor(public id: number) {}
+  constructor(public id: number) {}
 
-	async generateMenu() {
-		const res = await fetch(this.url, {
-			cf: {
-				cacheTtl: 86400,
-			},
-		});
+  async generateMenu() {
+    const res = await fetch(this.url, {
+      cf: {
+        cacheTtl: 86400,
+      },
+    })
 
-		const data: LunchmenyItem[] = await res.json();
+    const data: LunchmenyItem[] = await res.json()
 
-		const menu = data.reduce<Record<string, string>>((acc, item) => {
-			const clemDay = item.acf?.veckodag?.[0]?.value;
-			const dish = item.title?.rendered;
+    const menu = data.reduce<Record<string, string>>((acc, item) => {
+      const clemDay = item.acf?.veckodag?.[0]?.value
+      const dish = item.title?.rendered
 
-			if (clemDay && dish) {
-				const day = weekdayMapping[clemDay];
-				if (day) {
-					acc[day] = dish;
-				}
-			}
+      if (clemDay && dish) {
+        const day = weekdayMapping[clemDay]
+        if (day) {
+          acc[day] = dish
+        }
+      }
 
-			return acc;
-		}, {});
+      return acc
+    }, {})
 
-		this.menu = menu;
-		return menu;
-	}
+    this.menu = menu
+    return menu
+  }
 
-	async getDayMenu(weekday: string) {
-		return this.menu[weekday];
-	}
+  async getDayMenu(weekday: string) {
+    return this.menu[weekday]
+  }
 }
